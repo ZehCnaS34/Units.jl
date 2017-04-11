d2de0f8b1f7ba9ba1dcfb7969926d3f7d96c21a8
Alexander Sanchez <asanchez.dev@gmail.com>
2017-04-11 01:49:35 -0400

    Adding basic associations

--- src/Units.jl
+++ src/Units.jl
@@ -1,5 +1,39 @@
 module Units
 
+E  = 1e18
+P  = 1e15
+T  = 1e12
+G  = 1e9
+M  = 1e6
+k  = 1e3
+h  = 1e2
+da = 1e1
+d  = 1e-1
+c  = 1e-2
+m  = 1e-3
+u  = 1e-6
+n  = 1e-9
+p  = 1e-12
+f  = 1e-15
+a  = 1e-18
+
+
 # package code goes here
+export E,
+    P,
+    T,
+    G,
+    M,
+    k,
+    h,
+    da,
+    d,
+    c,
+    m,
+    u,
+    n,
+    p,
+    f,
+    a
 
 end # module
